798cf336abc3dc7f1d21fd80ae7a143bdffc2144
doyoung0205 <doyoung0205@naver.com>
2022-09-01 01:07:26 +0900

    add shut down test project

---
+++ shutdown-test/src/main/java/me/doyoung/shutdowntest/ShutdownTestApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.shutdowntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutdownTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShutdownTestApplication.class, args);
+    }
+
+}
---
+++ shutdown-test/src/test/java/me/doyoung/shutdowntest/ShutdownTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.shutdowntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShutdownTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
